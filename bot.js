c9dd0fc90517f60ba10ab3af5aa2d7625b3345d6
Alasdair Collinson <blalasaadri@googlemail.com>
2018-03-03 08:12:41 +0100

    Determine the last tweet by Seamus Gorman

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Twit = require('twit');
+const moment = require('moment');
+
+const config = require('./config');
+
+const twitter = new Twit({
+    ...config,
+    timeout_ms: 60*1000,
+});
+
+twitter.get('search/tweets', { 
+        q: `from:@gormanseamus -filter:retweets since:${moment().subtract(1, 'days').format('YYYY-MM-DD')}`, 
+        count: 100, 
+        result_type: 'recent'
+    }, (err, data, response) => {
+    console.log(`Found ${data.statuses.length} tweets in total`);
+    const nonReplyTweets = data.statuses
+        .filter(tweet => tweet.in_reply_to_status_id === null)
+        .filter(tweet => tweet.in_reply_to_user_id === null);
+    console.log(`Found ${nonReplyTweets.length} tweets that weren't replies`);
+    const minifiedTweets = nonReplyTweets.map(({ text, created_at }) => ({ text, created_at }));
+    const lastTweet = minifiedTweets[0];
+    console.log(JSON.stringify(lastTweet, null, 2));
+});
