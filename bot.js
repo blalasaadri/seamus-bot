f0c13bb8ff5e8c78b2c4c661977bb855a374a7c0
Alasdair Collinson <blalasaadri@googlemail.com>
2018-03-04 19:37:57 +0100

    Added timestamps to log outputs

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const Hapi = require('hapi');
 
 const config = require('./config');
 
+const LOG_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH:mm:ss Z';
 const retryIntervalInMs = 60*1000;
 const twitter = new Twit({
     ...config,
@@ -14,7 +15,7 @@ const twitter = new Twit({
 const possibleReplies = [
     '@gormanseamus Have you been hacked? :-( #DontHackSeamus',
     //'Oh dear, @gormanseamus may have been hacked. O_o #DontHackSeamus',
-    'Were you hacked, @gormanseamus? #DontHackSeamus',
+    //'Were you hacked, @gormanseamus? #DontHackSeamus',
     //'Hm... That doesn\'t sound like the real @gormanseamus. #DontHackSeamus',
     'Hey nasty H4xx0rs, leave @gormanseamus alone! #DontHackSeamus',
     'Could it be... that @gormanseamus... has been hacked? #DontHackSeamus',
@@ -66,11 +67,11 @@ setInterval(() =>
                     //console.log(`Tweet https://twitter.com/gormanseamus/status/${tweet.id_str} was posted on ${tweetTime} and I'm looking for tweets since ${lastCheck}. That means I will ${isTweetNew ? 'reply' : 'not reply'}.`);
                     return isTweetNew;
                 }).map(tweet => {
-                    console.log(`Replying to tweet: "${tweet.text}" (url: https://twitter.com/gormanseamus/status/${tweet.id_str})`);
+                    console.log(`[${moment().format(LOG_TIMESTAMP_FORMAT)}] Replying to tweet: "${tweet.text}" (url: https://twitter.com/gormanseamus/status/${tweet.id_str})`);
                     return twitter.post('statuses/update', { status: replyText(), in_reply_to_status_id: tweet.id_str });
                 }));
         })
         .catch(console.err)
-        .then(values => console.log(`${values.length} concerned replies sent`)),
+        .then(values => console.log(`[${moment().format(LOG_TIMESTAMP_FORMAT)}] ${values.length} concerned replies sent`)),
         retryIntervalInMs
     );
